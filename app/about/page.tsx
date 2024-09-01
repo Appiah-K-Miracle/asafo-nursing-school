519d0f818868ec40197c592df0dd43f54b47b301
Appiah-K-Miracle <amdwatkins123@gmail.com>
2024-09-01 04:41:30 +0000

    I have created the about page and still on it

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import AboutHeader from "@/components/aboutpages/aboutHeader";
 
 function AboutPage() {
     return (
-        <div>
-            <h1>About Page</h1>
-        </div>
+        <main>
+            <AboutHeader/>
+        </main>
     );
 }
 
