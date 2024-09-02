46c41efb6a67e9767078ec303eb41723d0cf7b9d
Appiah-K-Miracle <amdwatkins123@gmail.com>
2024-09-02 06:55:15 +0000

    Add another page

---
+++ app/academic/exam/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExamPage = () => {
+    return (
+        <div>
+            <h1>Examination page....</h1>
+        </div>
+    );
+};
+
+export default ExamPage;
