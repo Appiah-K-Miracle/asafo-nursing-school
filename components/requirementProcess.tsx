154649787e6e8405951c58a8ad34ef18b9e3193d
Appiah-K-Miracle <amdwatkins123@gmail.com>
2024-09-05 15:05:08 +0000

    added another item

--- components/requirementProcess.tsx
+++ components/requirementProcess.tsx
@@ -34,6 +34,23 @@ const RequirementProcess = () => {
                              </li>
                          </ul>
                      </AccordionContent>
+                 </AccordionItem> <AccordionItem value="item-2">
+                     <AccordionTrigger className="text-TeiColor font-bold capitalize text-lg">Requirement to fill out the form</AccordionTrigger>
+                     <AccordionContent>
+                         <ul className="list-disc px-8 text-TeiColor font-medium text-xl text-left">
+                             <li className="pb-2"> Serial Number
+                             </li>
+                             <li className="pb-2">Pin
+                             </li>
+                             <li className="pb-2">WASSCE Certificate/Nov-Dec</li>
+                             <li className="pb-2">Active Phone Number</li>
+                             <li className="pb-2">Passport Sized Photo
+                             <div className="bg-gray-200 text-TeiColor py-4 my-4 font-bold text-sm px-5 rounded-sm border-l-primaryColor border-l-8">
+                                 <p>Picture Dimension must be 450x450 pixels</p>
+                             </div>
+                             </li>
+                         </ul>
+                     </AccordionContent>
                  </AccordionItem>
              </Accordion>
 
